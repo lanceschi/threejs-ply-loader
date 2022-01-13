04da43931e97b5ad6a82197d42338fcc5a8a68ed
Luca Anceschi <luca.anceschi@gmail.com>
2022-01-13 16:57:04 +0100

    Adding missing files

---
+++ src/helpers/index.mjs
@@ -0,0 +1,19 @@
+/**
+ * Convert passed node Buffer to ArrayBuffer
+ * @param  {Buffer} buf
+ * @returns {ArrayBuffer}
+ */
+export const bufferToArrayBuffer = (buf) => {
+  if (Buffer.isBuffer(buf) === false) {
+    throw new Error(`Passed argument is not of type Buffer. Can't proceed`);
+  }
+
+  const ab = new ArrayBuffer(buf.length);
+  let view = new Uint8Array(ab);
+
+  for (let i = 0; i < buf.length; ++i) {
+    view[i] = buf[i];
+  }
+
+  return ab;
+};
---
+++ tests/index.spec.mjs
@@ -0,0 +1,70 @@
+import THREE from 'three';
+import fs from 'fs';
+import { expect } from 'chai';
+import { PLYLoaderFactory } from '../src/index.mjs';
+const PLYLoader = PLYLoaderFactory(THREE);
+
+describe('Library main test suite', () => {
+  it('PLYLoader should be imported correctly', () => {
+    expect(PLYLoader).to.be.a('function');
+    expect(new PLYLoader()).to.be.an.instanceof(PLYLoader);
+  });
+
+  it('Should instantiate correctly the PLYLoader object', () => {
+    const plyLoader = new PLYLoader();
+
+    expect(plyLoader).to.be.a('object');
+    expect(plyLoader).to.be.an.instanceof(PLYLoader);
+    expect(plyLoader).to.have.property('bufferToArrayBuffer');
+  });
+
+  it('PLYLoader instance should feature a bufferToArrayBuffer function', () => {
+    const plyLoader = new PLYLoader();
+
+    expect(plyLoader).to.have.property('bufferToArrayBuffer');
+    expect(plyLoader.bufferToArrayBuffer).to.be.a('function');
+  });
+
+  it('PLYLoader instance should feature a parse function', () => {
+    const plyLoader = new PLYLoader();
+
+    expect(plyLoader).to.have.property('parse');
+    expect(plyLoader.parse).to.be.a('function');
+  });
+
+  it('PLYLoader bufferToArrayBuffer should throw if argument is not a Buffer', () => {
+    const plyLoader = new PLYLoader();
+
+    const thowingFunc = () => plyLoader.bufferToArrayBuffer('Hahahah - ;)');
+
+    expect(thowingFunc).to.throw(Error);
+  });
+
+  it('PLYLoader bufferToArrayBuffer should convert Buffer to ArrayBuffer', () => {
+    const plyLoader = new PLYLoader();
+
+    // Read 3D Model as PLY file
+    const fileBuffer = fs.readFileSync('./examples/assets/cube.ply');
+
+    // Convert node file Buffer to ArrayBuffer
+    const fileArrayBuffer = plyLoader.bufferToArrayBuffer(fileBuffer);
+
+    expect(fileArrayBuffer).to.be.a('ArrayBuffer');
+  });
+
+  it('Parsed file should an instance of BufferGeometry', () => {
+    const plyLoader = new PLYLoader();
+
+    // Read 3D Model as PLY file
+    const fileBuffer = fs.readFileSync('./examples/assets/cube.ply');
+
+    // Convert node file Buffer to ArrayBuffer
+    const fileArrayBuffer = plyLoader.bufferToArrayBuffer(fileBuffer);
+
+    // Parse 3D model geometry
+    const geometry = plyLoader.parse(fileArrayBuffer);
+
+    expect(geometry).to.be.a('object');
+    expect(geometry).to.be.an.instanceof(THREE.BufferGeometry);
+  });
+});
